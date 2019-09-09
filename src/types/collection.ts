b770b44387a80eddbc946c3a7b208d810f4aa78f
Ken <yukanliao@gmail.com>
2019-09-09 13:21:02 -0400

    Moved API from blockstack.js to the collection abstract class.

--- src/types/collection.ts
+++ src/types/collection.ts
@@ -1,3 +1,13 @@
+import { 
+  UserSession, 
+  getFile,
+  putFile,
+  deleteFile,
+  listFilesLoop
+} from 'blockstack'
+
+export const COLLECTION_GAIA_PREFIX = 'collection'
+
 export interface SchemaAttribute {
   type: string | Record<string, any> | any[] | number | boolean;
 }
@@ -41,12 +51,66 @@ export abstract class Collection implements Serializable {
     return `${COLLECTION_SCOPE_PREFIX}${this.collectionName}`
   }
 
-  static fromData(data: string) {
+  public collectionName(): string {
+    throw new Error('Required abstract method collectionName was not implemented')
+  }
+
+  static fromData(data: string | ArrayBuffer) {
     throw new Error('Required abstract method fromData was not implemented')
   }
 
-  public collectionName(): string {
-    throw new Error('Required abstract method collectionName was not implemented')
+  static async get(identifier: string, userSession?: UserSession) {
+    userSession = userSession || new UserSession()
+    let hubConfig = await userSession.getCollectionGaiaHubConnection(this.collectionName)
+    let normalizedIdentifier = COLLECTION_GAIA_PREFIX + '/' + identifier
+    let opt = {
+      gaiaHubConfig: hubConfig
+    }
+    return getFile(normalizedIdentifier, opt, userSession)
+      .then((fileContent) => {
+        if (fileContent) {
+          return this.fromData(fileContent)
+        } else {
+          throw new Error('Collection item not found')
+        }
+      })
+  }
+
+  static async list(callback: (name: string) => boolean, userSession?: UserSession) {
+    userSession = userSession || new UserSession()
+    let hubConfig = await userSession.getCollectionGaiaHubConnection(this.collectionName)
+    return listFilesLoop(userSession, hubConfig, null, 0, 0, (name) => {
+      let collectionGaiaPathPrefix = COLLECTION_GAIA_PREFIX + '/'
+      if (name.startsWith(collectionGaiaPathPrefix)) {
+        // Remove collection/ prefix from file names
+        let identifier = name.substr(collectionGaiaPathPrefix.length)
+        return callback(identifier)
+      } else {
+        // Skip non-collection prefix files
+        return true
+      }
+    })
+  }
+
+  async save(userSession?: UserSession) {
+    userSession = userSession || new UserSession()
+    let hubConfig = await userSession.getCollectionGaiaHubConnection(this.collectionName())
+    let file = this.serialize()
+    let normalizedIdentifier = COLLECTION_GAIA_PREFIX + '/' + this.attrs.identifier
+    let opt = {
+      gaiaHubConfig: hubConfig
+    }
+    return putFile(normalizedIdentifier, file, opt, userSession)
+  }
+
+  async delete(userSession?: UserSession) {
+    userSession = userSession || new UserSession()
+    let hubConfig = await userSession.getCollectionGaiaHubConnection(this.collectionName())
+    let opt = {
+      gaiaHubConfig: hubConfig
+    }
+    const normalizedIdentifier = COLLECTION_GAIA_PREFIX + '/' + this.attrs.identifier
+    return deleteFile(normalizedIdentifier, opt, userSession)
   }
 
   abstract serialize()
