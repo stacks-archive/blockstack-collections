9c35a9ee1efbb1ed87a4dc045d92f6f648c22941
Matthew Little <zone117x@gmail.com>
2019-09-12 17:19:16 -0400

    * Fix UMD bundle generation. * Create simple UMD file loader test page.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const fs = require('fs');
 
 module.exports = (env, argv) => {
 
   env = env || {};
   argv = argv || {};
+  const isEnvAnalyze = process.env.ANALYZE || env.ANALYZE;
   const isEnvDev = [process.env, env.NODE_ENV, argv.mode].includes('development') || env.development;
   const isEnvTest = [process.env, env.NODE_ENV, argv.mode].includes('test') || env.test;
   const isEnvProd = [process.env, env.NODE_ENV, argv.mode].includes('production') || env.production;
@@ -22,23 +22,29 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
-          test: /\.ts?$/,
-          exclude: /node_modules/,
+          test: /\.ts$/,
+          exclude: /(node_modules|bower_components)/,
           use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', {
+                    targets: '> 2%',
+                    useBuiltIns: 'usage',
+                    corejs: { version: 3 },
+                  }]
+                ]
+              }
+            },
             {
               loader: 'ts-loader',
               options: {
-                configFile: "tsconfig.json"
+                configFile: 'tsconfig.browser.json',
+                transpileOnly: true
               }
             }
           ]
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
         }
       ].concat((isEnvDev || isEnvTest) ? {
         test: /\.js$/,
@@ -55,11 +61,22 @@ module.exports = (env, argv) => {
     output: {
       filename: 'blockstack-collections.js',
       path: path.resolve(__dirname, 'dist'),
-      library: 'blockstack-collections',
+      library: 'blockstackCollections',
       libraryTarget: 'umd',
       globalObject: 'this'
     },
-    plugins: []
+    plugins: [],
+    devServer: {
+      contentBase: [__dirname, path.join(__dirname, 'dist')],
+      index: path.join(__dirname, 'index.html'),
+      host: '127.0.0.1',
+      port: 9134,
+      open: true
+    }
+  }
+
+  if (isEnvAnalyze) {
+    opts.plugins.push(new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)())
   }
 
   return opts;
